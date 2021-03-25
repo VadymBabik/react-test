7c7056de0fbe50df09261f77cfd87f2ddba45aeb
Vadym Babik <vadym.babik@gmail.com>
2021-03-25 09:26:26 +0200

    start

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                    Hello react
+                </h1>
+            </header>
+        </div>
+    );
+}
+
